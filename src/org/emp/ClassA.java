1ba54c2173b49b15d20e6903216a1366311b0232
SrihariMahendran <Sreeharry1996@gmail.com>
2025-02-26 21:44:58 +0530

    Created class A and B and modified sysout in Trail page

---
+++ src/org/emp/ClassA.java
@@ -0,0 +1,17 @@
+package org.emp;
+
+public class ClassA {
+
+	
+	private void method1() {
+		System.out.println("1");
+		
+		
+
+	}
+	
+	
+	private void method2() {
+		System.out.println("2");
+}
+}
---
+++ src/org/emp/ClassB.java
@@ -0,0 +1,19 @@
+package org.emp;
+
+public class ClassB {
+	
+	private void method3() {
+		System.out.println("3");
+
+		
+
+}
+
+	
+	private void method4() {
+	
+		System.out.println("4");
+		
+	}
+	
+}
